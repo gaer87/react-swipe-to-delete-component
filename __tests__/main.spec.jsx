392b2eec9fbd66a65b796d27f796e767fa398eb6
Fedotov Alexander <gaer87@gmail.com>
2017-01-10 22:28:25 +0300

    Made snapshot test

--- __tests__/main.spec.jsx
+++ __tests__/main.spec.jsx
@@ -1,10 +1,13 @@
-import react from 'react';
+import React from 'react';
 import SwipeToDelete from '../src/js/main';
 import renderer from 'react-test-renderer';
 
 
 describe('A SwipeToDelete', () => {
   it('should renders correctly', () => {
+    // In componentDidMount requires a DOM
+    SwipeToDelete.prototype.componentDidMount = jest.fn();
+
     const component = renderer.create(
       <SwipeToDelete><div>Content ...</div></SwipeToDelete>
     );
