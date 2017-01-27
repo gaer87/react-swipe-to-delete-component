10f0c706e170dd3bacd6407fd1789ce46dae0761
Fedotov Alexander <gaer87@gmail.com>
2017-01-27 09:18:12 +0300

    Delete old tests

--- test/utils/isMobileSpec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import isMobile from '../../src/js/utils/isMobile';
-
-describe('The isMobule utils', function () {
-	it('check mobile', function () {
-		expect(isMobile.any()).toBeFalsy();
-	});
-});
